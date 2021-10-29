1a9c3058f35759073f1bd300e51d7927c70a91c8
MrTryAgain <mrtryagain2021@gmail.com>
2021-10-29 23:27:31 +0200

    commit mit halber saas installation vor dem bau neuer seiten freitag abend lol

---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+/*const { backgroundColor } = require("tailwindcss/defaultTheme");*/
+
+/* Back to top button */
+back2top.addEventListener('click', (e) => {
+e.preventDefault(); /* unterbinden dr standart aktion */
+window.scroll({ top: 0, left:0, behavior: 'smooth'});/* dann gehe zu back top position angabe */
+});
+
+/* Uhr Boris */
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const uhr = document.querySelectorAll('.uhr')
+console.log(uhr)/* loggen */
+ const time = () => {
+  var d = new Date()
+  var s = d.getSeconds()
+  var m = d.getMinutes()
+  var h = d.getHours()
+
+
+  uhr.forEach(uhr => uhr.innerHTML = `${('0' + h).substr(-2)}:${('0' + m).substr(-2)}:${('0' + s).substr(-2)} Uhr`)
+}
+
+setInterval(time, 1000)
+
+
+
+})
